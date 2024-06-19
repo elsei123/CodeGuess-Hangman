60146fb61eeba3a63e6c237a08ba0d176e833d7a
elsei123 <eltonseidi@gmail.com>
2024-06-19 12:08:37 +0000

    first commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Project: CodeGuess Hangman
+Description: A word guessing game where the user tries to guess the secret word letter by letter
+"""
+
+import random
+
+def get_random_word():
+    """
+    Get a random word from a predefined list.
+    """
+    words = ['python', 'hangman', 'challenge', 'programming', 'openai']
+    return random.choice(words)
